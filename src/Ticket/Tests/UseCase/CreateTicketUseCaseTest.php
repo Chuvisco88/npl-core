e63b704097c8854fb66c0df22f9feb59b933ddd2
Fabian Schweizer <fa.schweizer@gmail.com>
2016-08-19 08:48:50 +0200

    Refactored CreateTicketUseCase
    * use stubs now
    * used separate functions for setting up the repositories

--- src/Ticket/Tests/UseCase/CreateTicketUseCaseTest.php
+++ src/Ticket/Tests/UseCase/CreateTicketUseCaseTest.php
@@ -4,53 +4,50 @@ namespace Npl\Ticket\UseCase;
 
 use Npl\Lan\Entity\LanEntity;
 use Npl\Lan\Exception\LanNotFoundException;
-use Npl\Lan\Tests\Fake\Repository\FakeLanRepository;
-use Npl\Ticket\Tests\Fake\Repository\FakeTicketRepository;
+use Npl\Lan\Repository\LanRepositoryInterface;
+use Npl\Ticket\Entity\TicketEntity;
+use Npl\Ticket\Repository\TicketRepositoryInterface;
 use Npl\Ticket\Tests\Fake\Request\FakeCreateTicketRequest;
 use Npl\Ticket\Tests\Fake\Response\FakeCreateTicketResponse;
 use Npl\Ticket\Tests\Fake\ViewFactory\FakeTicketViewFactory;
 use Npl\User\Entity\UserEntity;
 use Npl\User\Exception\UserNotFoundException;
-use Npl\User\Tests\Fake\Repository\FakeUserRepository;
+use Npl\User\Repository\UserRepositoryInterface;
 
 class CreateTicketUseCaseTest extends \PHPUnit_Framework_TestCase
 {
-    const INVALID_LAN_ID = 0;
-    const INVALID_USER_ID = 0;
-    const MAX_NUMBER_OF_TICKETS = 5;
-    const NUMBER_OF_TICKETS = 5;
     const LAN_ID = 1;
     const USER_ID = 1;
 
     private $_lan;
     private $_lanName = 'noprobLAN vX.Y';
-    private $_lans = [];
+
     private $_requestLanId;
     private $_requestUserId;
     private $_user;
     private $_userNickname = 'User Nickname';
-    private $_users = [];
-    private $_tickets = [];
+
+    private $_lanRepository;
+    private $_userRepository;
+    private $_ticketRepository;
 
     public function setUp()
     {
-        $this->_lan = new LanEntity($this->_lanName, new \DateTime('yesterday 18:00'), new \DateTime('tomorrow 16:00'));
-        $this->_lan->setId(self::LAN_ID);
-        $this->_lans[] = $this->_lan;
+        $this->_setupLanEntity();
+        $this->_setupUserEntity();
 
         $this->_requestLanId = self::LAN_ID;
         $this->_requestUserId = self::USER_ID;
-
-        $this->_user = new UserEntity();
-        $this->_user->setId(self::USER_ID);
-        $this->_user->setNickname($this->_userNickname);
-        $this->_users[] = $this->_user;
 
         $this->_tickets = [];
     }
 
     public function testCanCreateTicket()
     {
+        $this->_setupLanRepository();
+        $this->_setupUserRepository();
+        $this->_setupTicketRepository();
+
         $response = $this->processUseCase();
 
         static::assertSame($this->_userNickname, $response->getBuyerName());
@@ -60,20 +57,81 @@ class CreateTicketUseCaseTest extends \PHPUnit_Framework_TestCase
 
     public function testLanNotFoundException()
     {
-        $this->setExpectedException(LanNotFoundException::class);
+        $this->_setupLanRepository(true);
+        $this->_setupUserRepository();
+        $this->_setupTicketRepository();
 
-        $this->_requestLanId = self::INVALID_LAN_ID;
+        $this->setExpectedException(LanNotFoundException::class);
 
         $this->processUseCase();
     }
 
     public function testUserNotFoundException()
     {
+        $this->_setupLanRepository();
+        $this->_setupUserRepository(true);
+        $this->_setupTicketRepository();
+
         $this->setExpectedException(UserNotFoundException::class);
 
-        $this->_requestUserId = self::INVALID_USER_ID;
 
         $this->processUseCase();
+    }
+
+    private function _setupLanEntity()
+    {
+        $this->_lan = new LanEntity(
+            $this->_lanName,
+            new \DateTime('yesterday 18:00'),
+            new \DateTime('tomorrow 16:00')
+        );
+        $this->_lan->setId(self::LAN_ID);
+    }
+
+    private function _setupUserEntity()
+    {
+        $this->_user = new UserEntity();
+        $this->_user->setId(self::USER_ID);
+        $this->_user->setNickname($this->_userNickname);
+    }
+
+    private function _setupLanRepository($shouldThrowNotFoundError = false)
+    {
+        $this->_lanRepository = $this->getMock(LanRepositoryInterface::class);
+        if ($shouldThrowNotFoundError) {
+            $this->_lanRepository->method('findById')
+                ->willThrowException(new LanNotFoundException());
+        } else {
+            $this->_lanRepository->method('findById')
+                ->willReturn($this->_lan);
+        }
+    }
+
+    private function _setupUserRepository($shouldThrowNotFoundError = false)
+    {
+        $this->_userRepository = $this->getMock(UserRepositoryInterface::class);
+        if ($shouldThrowNotFoundError) {
+            $this->_userRepository->method('findById')
+                ->willThrowException(new UserNotFoundException());
+        } else {
+            $this->_userRepository->method('findById')
+                ->willReturn($this->_user);
+        }
+    }
+
+    private function _setupTicketRepository($addNumberOfTickets = 0)
+    {
+        $this->_ticketRepository = $this->getMock(TicketRepositoryInterface::class);
+        $tickets = [];
+
+        for ($ticketId = 1; $ticketId <= $addNumberOfTickets; $ticketId++) {
+            $ticket = new TicketEntity($this->_user, $this->_lan);
+            $ticket->setId($ticketId + 1);
+            $tickets[] = $ticket;
+        }
+
+        $this->_ticketRepository->method('findByUserId')
+            ->willReturn($tickets);
     }
 
     /**
@@ -81,14 +139,17 @@ class CreateTicketUseCaseTest extends \PHPUnit_Framework_TestCase
      */
     private function processUseCase()
     {
-        $lanRepository = new FakeLanRepository($this->_lans);
-        $ticketRepository = new FakeTicketRepository($this->_tickets);
         $ticketViewFactory = new FakeTicketViewFactory();
-        $userRepository = new FakeUserRepository($this->_users);
         $request = new FakeCreateTicketRequest($this->_requestLanId, $this->_requestUserId);
         $response = new FakeCreateTicketResponse();
 
-        $useCase = new CreateTicketUseCase($lanRepository, $ticketRepository, $ticketViewFactory, $userRepository);
+        $useCase = new CreateTicketUseCase(
+            $this->_lanRepository,
+            $this->_ticketRepository,
+            $ticketViewFactory,
+            $this->_userRepository
+        );
+
         $useCase->process($request, $response);
 
         return $response;
