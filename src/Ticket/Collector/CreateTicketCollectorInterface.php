0faac161826d1181152f0086e8845bbf6c5d8079
Fabian Schweizer <fa.schweizer@gmail.com>
2016-08-20 15:29:28 +0200

    Added docblock to CreateTicketCollectorInterface

--- src/Ticket/Collector/CreateTicketCollectorInterface.php
+++ src/Ticket/Collector/CreateTicketCollectorInterface.php
@@ -2,10 +2,42 @@
 
 namespace Npl\Ticket\Collector;
 
+use Npl\Lan\Entity\LanEntity;
+use Npl\Lan\Exception\LanNotFoundException;
+use Npl\Ticket\Entity\TicketEntity;
+use Npl\User\Entity\UserEntity;
+use Npl\User\Exception\UserNotFoundException;
+
 interface CreateTicketCollectorInterface
 {
+    /**
+     * @param $lanId
+     *
+     * @return LanEntity
+     * @throws LanNotFoundException
+     */
     public function findLanById($lanId);
+
+    /**
+     * @param $userId
+     *
+     * @return UserEntity
+     * @throws UserNotFoundException
+     */
     public function findUserById($userId);
+
+    /**
+     * @param $lanId
+     *
+     * @return TicketEntity[]
+     */
     public function findTicketsByLan($lanId);
+
+    /**
+     * @param $lanId
+     * @param $userId
+     *
+     * @return TicketEntity[]
+     */
     public function findTicketsByLanAndUser($lanId, $userId);
 }
