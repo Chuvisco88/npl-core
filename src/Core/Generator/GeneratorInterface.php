1e36a3c2d90a9721371e0b0b288494767aae6b7a
Fabian Schweizer <fa.schweizer@gmail.com>
2016-08-20 17:35:11 +0200

    Implemented a TicketGenerator

---
+++ src/Core/Generator/GeneratorInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Npl\Core\Generator;
+
+interface GeneratorInterface
+{
+    /**
+     * @return mixed
+     */
+    public function generate();
+}
---
+++ src/Ticket/Generator/TicketGenerator.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Npl\Ticket\Generator;
+
+use Npl\Core\Generator\GeneratorInterface;
+use Npl\Ticket\Entity\TicketEntity;
+
+class TicketGenerator implements GeneratorInterface
+{
+    private $_lan;
+    private $_buyer;
+    private $_holder;
+    private $_numberOfTickets;
+
+    public function __construct($ticketConfiguration, $numberOfTickets = 0)
+    {
+        $this->_lan = $ticketConfiguration->getLan();
+        $this->_buyer = $ticketConfiguration->getBuyer();
+        $this->_holder = $ticketConfiguration->getHolder();
+        $this->_numberOfTickets = $numberOfTickets;
+    }
+
+    /**
+     * @return TicketEntity[]
+     */
+    public function generate()
+    {
+        $tickets = [];
+
+        for ($ticketId = 1; $ticketId <= $this->_numberOfTickets; $ticketId++) {
+            $ticket = new TicketEntity($this->_buyer, $this->_lan, $this->_holder);
+            $ticket->setId($ticketId);
+            $tickets[] = $ticket;
+        }
+
+        return $tickets;
+    }
+}
